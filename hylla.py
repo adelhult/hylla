94a49c2373ddc440b1f6633214cce581234ab5f4
Eli Adelhult <jxz2000@hotmail.se>
2019-04-10 13:25:54 +0200

    added a docstring to hylla.py

--- hylla.py
+++ hylla.py
@@ -1,3 +1,14 @@
+"""Hylla - Organize all your projects from the command-line
+
+Usage:
+hylla [OPTIONS] COMMAND [ARGS]
+If Hylla is used without specifying a command, help information will be printed.
+Complete documentation can also be found on https://github.com/adelhult/hylla or
+by using the command 'hylla docs'.
+
+Hylla is created by Eli Adelhult licensed under the MIT license
+"""
+
 import os
 import sys
 import shutil
