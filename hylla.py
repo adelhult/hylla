c2739c21035afd178eef9ed0be998cb876ccffce
Eli Adelhult <jxz2000@hotmail.se>
2019-05-03 13:04:16 +0200

    todo

--- hylla.py
+++ hylla.py
@@ -9,6 +9,13 @@ by using the command 'hylla docs'.
 Hylla is a project by Eli Adelhult and is licensed under the MIT license.
 """
 
+# todo:
+# add a remove and edit method to the Project class
+# create the function commands(code)
+# fix the edit function
+# add help attributes to the click options and arguments
+
+
 import os
 import sys
 import shutil
